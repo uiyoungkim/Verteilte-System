d055f27f6c81f3a74eb9541e4e2459dd01aa549c
zxTyphoon <seantylerstraub@gmail.com>
2024-04-02 03:08:57 +0200

    Added cli input logic

--- src/broker/UdpClient.java
+++ src/broker/UdpClient.java
@@ -1,21 +1,72 @@
 package broker;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.net.*;
+import java.io.*;
 
 public class UdpClient {
-// Client für Hotel
     public static void main(String[] args) {
-        String anfrageDaten = String.format(
-                "{\"typ\":\"Hotelbuchung\", \"hotelName\":\"FluchtAnDieSee\", \"checkInDatum\":\"%s\", \"checkOutDatum\":\"%s\"}",
-                "2023-05-01",
-                "2023-05-05"
-        ); // --> JSON ähnliche String
+        String hotel = "";
+        int hotelzimmer = 0;
+        int hoteldauer = 0;
+        String hotelankunft = "";
+        String flug = "";
+        int flugplaetze = 0;
+        int reisedauer = 0;
+        String abflug = "";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Geben Sie bitte Ihre Daten an. Benutzen Sie dafür bitte folgenden Argumente: -hotel String -hotelzimmer int -hoteldauer int -hotelankunft Date -flug String -flugplaetze int -reisedauer int -abflug Date : ");
+        try{
+        String eingabe = reader.readLine();
+        boolean valid = false;
+        while (!valid) {
+            String[] eingabeArray = eingabe.split("-");
+            for (int i = 1; i < eingabeArray.length; i++) {
+                String[] parameter = eingabeArray[i].trim().split(" ", 2);
+                switch (parameter[0]) {
+                    case "hotel":
+                        hotel = parameter[1];
+                        break;
+                    case "hotelzimmer":
+                        hotelzimmer = Integer.parseInt(parameter[1].trim());
+                        break;
+                    case "hoteldauer":
+                        hoteldauer = Integer.parseInt(parameter[1].trim());
+                        break;
+                    case "hotelankunft":
+                        hotelankunft = parameter[1];
+                        break;
+                    case "flug":
+                        flug = parameter[1];
+                        break;
+                    case "flugplaetze":
+                        flugplaetze = Integer.parseInt(parameter[1].trim());
+                        break;
+                    case "reisedauer":
+                        reisedauer = Integer.parseInt(parameter[1].trim());
+                        break;
+                    case "abflug":
+                        abflug = parameter[1];
+                        break;
+                    default:
+                        System.out.println("Unrecognized argument: " + parameter[0]);
+                        break;
+                }
+            }
+            if (!hotel.isEmpty() && hotelzimmer != 0 && hoteldauer != 0 && hotelankunft != null && !flug.isEmpty() && flugplaetze != 0 && reisedauer != 0 && abflug != null) {
+                valid = true;
+            } else {
+                System.out.println("Bitte geben Sie alle Daten an.");
+                eingabe = reader.readLine();
+            }
+        }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String anfrageDaten = String.format("{\"hotel\":\"%s\", \"hotelzimmer\":%d, \"hoteldauer\":%d, \"hotelankunft\":\"%s\", \"flug\":\"%s\", \"flugplaetze\":%d, \"reisedauer\":%d, \"abflug\":\"%s\"}", hotel, hotelzimmer, hoteldauer, hotelankunft, flug, flugplaetze, reisedauer, abflug);
 
         String serverAdresse = "localhost";
         int serverPort = 4445;
-
         UdpClient client = new UdpClient();
         client.sendeAnfrage(anfrageDaten, serverAdresse, serverPort);
     }
