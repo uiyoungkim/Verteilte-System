2ffeb8a700dad0402b655870d7f381225e97a5c4
Uiyoung Kim <s221683@student.dhbw-mannheim.de>
2024-04-03 18:07:07 +0200

    verbesserte Anfrage format

--- src/broker/UdpClient.java
+++ src/broker/UdpClient.java
@@ -15,58 +15,59 @@ public class UdpClient {
         String abflug = "";
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Geben Sie bitte Ihre Daten an. Benutzen Sie dafür bitte folgenden Argumente: -hotel String -hotelzimmer int -hoteldauer int -hotelankunft Date -flug String -flugplaetze int -reisedauer int -abflug Date : ");
-        try{
-        String eingabe = reader.readLine();
-        boolean valid = false;
-        while (!valid) {
-            String[] eingabeArray = eingabe.split("-");
-            for (int i = 1; i < eingabeArray.length; i++) {
-                String[] parameter = eingabeArray[i].trim().split(" ", 2);
-                switch (parameter[0]) {
-                    case "hotel":
-                        hotel = parameter[1];
-                        break;
-                    case "hotelzimmer":
-                        hotelzimmer = Integer.parseInt(parameter[1].trim());
-                        break;
-                    case "hoteldauer":
-                        hoteldauer = Integer.parseInt(parameter[1].trim());
-                        break;
-                    case "hotelankunft":
-                        hotelankunft = parameter[1];
-                        break;
-                    case "flug":
-                        flug = parameter[1];
-                        break;
-                    case "flugplaetze":
-                        flugplaetze = Integer.parseInt(parameter[1].trim());
-                        break;
-                    case "reisedauer":
-                        reisedauer = Integer.parseInt(parameter[1].trim());
-                        break;
-                    case "abflug":
-                        abflug = parameter[1];
-                        break;
-                    default:
-                        System.out.println("Unrecognized argument: " + parameter[0]);
-                        break;
+        try {
+            String eingabe = reader.readLine();
+            boolean valid = false;
+            while (!valid) {
+                String[] eingabeArray = eingabe.split("-");
+                for (int i = 1; i < eingabeArray.length; i++) {
+                    String[] parameter = eingabeArray[i].trim().split(" ", 2);
+                    switch (parameter[0]) {
+                        case "hotel":
+                            hotel = parameter[1];
+                            break;
+                        case "hotelzimmer":
+                            hotelzimmer = Integer.parseInt(parameter[1].trim());
+                            break;
+                        case "hoteldauer":
+                            hoteldauer = Integer.parseInt(parameter[1].trim());
+                            break;
+                        case "hotelankunft":
+                            hotelankunft = parameter[1];
+                            break;
+                        case "flug":
+                            flug = parameter[1];
+                            break;
+                        case "flugplaetze":
+                            flugplaetze = Integer.parseInt(parameter[1].trim());
+                            break;
+                        case "reisedauer":
+                            reisedauer = Integer.parseInt(parameter[1].trim());
+                            break;
+                        case "abflug":
+                            abflug = parameter[1];
+                            break;
+                        default:
+                            System.out.println("Unrecognized argument: " + parameter[0]);
+                            break;
+                    }
+                }
+                if (!hotel.isEmpty() && hotelzimmer != 0 && hoteldauer != 0 && !hotelankunft.isEmpty() && !flug.isEmpty() && flugplaetze != 0 && reisedauer != 0 && !abflug.isEmpty()) {
+                    valid = true;
+                } else {
+                    System.out.println("Bitte geben Sie alle Daten korrekt an.");
+                    eingabe = reader.readLine();
                 }
             }
-            if (!hotel.isEmpty() && hotelzimmer != 0 && hoteldauer != 0 && hotelankunft != null && !flug.isEmpty() && flugplaetze != 0 && reisedauer != 0 && abflug != null) {
-                valid = true;
-            } else {
-                System.out.println("Bitte geben Sie alle Daten an.");
-                eingabe = reader.readLine();
-            }
-        }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        String anfrageDaten = String.format("{\"hotel\":\"%s\", \"hotelzimmer\":%d, \"hoteldauer\":%d, \"hotelankunft\":\"%s\", \"flug\":\"%s\", \"flugplaetze\":%d, \"reisedauer\":%d, \"abflug\":\"%s\"}", hotel, hotelzimmer, hoteldauer, hotelankunft, flug, flugplaetze, reisedauer, abflug);
+
+        String anfrageDaten = String.format("{\"Hotel\": {\"hotel\":\"%s\", \"hotelzimmer\":%d, \"hoteldauer\":%d, \"hotelankunft\":\"%s\"}, \"Flug\": {\"flug\":\"%s\", \"flugplaetze\":%d, \"reisedauer\":%d, \"abflug\":\"%s\"}}", hotel, hotelzimmer, hoteldauer, hotelankunft, flug, flugplaetze, reisedauer, abflug);
 
         String serverAdresse = "localhost";
-        int serverPort = 4445;
+        int serverPort = 7777; // Port des MessageBrokers
         UdpClient client = new UdpClient();
         client.sendeAnfrage(anfrageDaten, serverAdresse, serverPort);
     }
@@ -87,7 +88,6 @@ public class UdpClient {
             socket.receive(packet);
             String empfangeneDaten = new String(packet.getData(), 0, packet.getLength());
             System.out.println("Antwort empfangen: " + empfangeneDaten);
-
         } catch (Exception e) {
             System.err.println("Fehler beim Senden oder Empfangen des Pakets: " + e.getMessage());
             e.printStackTrace();
